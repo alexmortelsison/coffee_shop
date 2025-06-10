32327b6c2b254b37ba2d457f9ae084254b81c290
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-06-10 09:23:16 +0900

    added bottom_nav_bar

---
+++ lib/components/bottom_nav_bar.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class MyButtonNavbar extends StatelessWidget {
+  const MyButtonNavbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(25),
+      child: GNav(
+        color: Colors.grey[400],
+        mainAxisAlignment: MainAxisAlignment.center,
+        activeColor: Colors.grey[700],
+        backgroundColor: Colors.grey.shade300,
+        tabBorderRadius: 24,
+        tabActiveBorder: Border.all(color: Colors.white),
+        tabs: const [
+          GButton(
+            icon: Icons.home,
+            text: "Shop",
+          ),
+          GButton(
+            icon: Icons.shopping_bag_outlined,
+            text: "Cart",
+          ),
+        ],
+      ),
+    );
+  }
+}
