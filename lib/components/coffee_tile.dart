0fdc68c0e2cd9e8a7cf994d4b7f5de0320f25919
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-06-10 11:52:26 +0900

    updated coffeetile

--- lib/components/coffee_tile.dart
+++ lib/components/coffee_tile.dart
@@ -3,14 +3,25 @@ import 'package:flutter/material.dart';
 
 class CoffeeTile extends StatelessWidget {
   final Coffee coffee;
-  const CoffeeTile({super.key, required this.coffee});
+  final Function()? onPressed;
+  const CoffeeTile({super.key, required this.coffee, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(coffee.name),
-      subtitle: Text(coffee.price),
-      leading: Image.asset(coffee.imagePath),
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.grey[200], borderRadius: BorderRadius.circular(12)),
+      margin: const EdgeInsets.only(bottom: 10),
+      padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 10),
+      child: ListTile(
+        title: Text(coffee.name),
+        subtitle: Text(coffee.price),
+        leading: Image.asset(coffee.imagePath),
+        trailing: IconButton(
+          icon: const Icon(Icons.add),
+          onPressed: onPressed,
+        ),
+      ),
     );
   }
 }
