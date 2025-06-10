0f549ac9ca055e8e56056ca406b5c1dd539de599
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-06-10 11:20:00 +0900

    updated coffeeshop model

---
+++ lib/models/coffee_shop.dart
@@ -0,0 +1,40 @@
+import 'package:coffee_shop/models/coffee.dart';
+
+class CoffeeShop {
+  final List<Coffee> _shop = [
+    Coffee(
+      name: "Long Black",
+      price: "4.10",
+      imagePath: "lib/images/black.png",
+    ),
+    Coffee(
+      name: "Latte",
+      price: "4.20",
+      imagePath: "lib/images/latte.png",
+    ),
+    Coffee(
+      name: "Espresso",
+      price: "3.50",
+      imagePath: "lib/images/espresso.png",
+    ),
+    Coffee(
+      name: "Iced Coffee",
+      price: "4.40",
+      imagePath: "lib/images/iced_coffee.png",
+    ),
+  ];
+
+  final List<Coffee> _userCart = [];
+
+  List<Coffee> get coffeeShop => _shop;
+
+  List<Coffee> get userCart => _userCart;
+
+  void addItemToCart(Coffee coffee) {
+    _userCart.add(coffee);
+  }
+
+  void removeItemToCart(Coffee coffee) {
+    _userCart.remove(coffee);
+  }
+}
